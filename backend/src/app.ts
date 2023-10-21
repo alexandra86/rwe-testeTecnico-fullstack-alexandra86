36883f8df9d8a71a297c6c4579a2da0a430445a0
Alexandra Miranda <alexyamiranda7@gmail.com>
2023-10-21 11:11:23 -0300

    feat: prefixo das rotas de login e usuário no app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,12 +2,18 @@ import cors from "cors";
 import "express-async-errors";
 import express, { Application } from "express";
 import { handleErrors } from "./errors";
+import { loginRoutes } from "./routers/login.routes";
+import { usersRoutes } from "./routers/users.routes";
 
 const app: Application = express();
 
 app.use(cors());
 
 app.use(express.json());
+
+app.use("/login", loginRoutes);
+
+app.use("/users", usersRoutes);
 
 app.use(handleErrors);
 

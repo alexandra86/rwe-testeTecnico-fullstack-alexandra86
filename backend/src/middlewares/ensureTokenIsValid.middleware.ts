af7a1bda1a7564b488997eadbce8045d713bb02c
Alexandra Miranda <alexyamiranda7@gmail.com>
2023-10-21 10:18:53 -0300

    feat: middleware de validação do token do usuário

---
+++ backend/src/middlewares/ensureTokenIsValid.middleware.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "../errors";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
+export const ensureTokenIsValidMiddleware = async (
+  request: Request,
+  _: Response,
+  next: NextFunction
+): Promise<void> => {
+  let token = request.headers.authorization;
+
+  if (!token) {
+    throw new AppError("Missing bearer token", 401);
+  }
+
+  token = token.split(" ")[1];
+
+  jwt.verify(token, process.env.SECRET_KEY!, (error, decoded: any) => {
+    if (error) {
+      throw new AppError(error.message, 401);
+    }
+
+    request.user = {
+      id: decoded.sub,
+    };
+
+    return next();
+  });
+};
